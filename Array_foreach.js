c76932c516ef466de4cd571e18fe183fc49b110b
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-02-16 18:11:17 +0100

    Update Array_foreach.js

--- Array_foreach.js
+++ Array_foreach.js
@@ -4,13 +4,13 @@ grades.forEach(function(grade) {      //singolar
     // do something with individual grade       //The .forEach(callback) method allows you to run the callback function for every item in that array.
     console.log(grade);                
 });
+
+
 //another example
 const grades = [10, 14, 15]; // array (plural)
 grades.forEach(function(grade) { // array item (singular)
     console.log(grade);
 });
-
-
 const people = ["Sam", "Alex"]; // array (plural)
 people.forEach(function(person) { // array item (singular)
     console.log(person);
