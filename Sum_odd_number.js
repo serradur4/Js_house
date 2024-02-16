405b796e0454c5082c4d99bf607a64b0ca1e7ca7
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-02-16 17:40:28 +0100

    Create Sum_odd_number.js

---
+++ Sum_odd_number.js
@@ -0,0 +1,13 @@
+function sumOddNumbers(numbers) {
+    let sum = 0;
+    numbers.forEach(function(number){
+        if (number % 2 !== 0){
+            sum += number;
+        }
+    })
+    return sum;
+
+}
+//console.log(sumOddNumbers([15, 5, 10])); // 20
+//console.log(sumOddNumbers([2, 3, 4, 5, 6])); // 8
+//console.log(sumOddNumbers([-2, -3, 4, 5, 6])); // 2
