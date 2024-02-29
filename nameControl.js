67f0cf6fd02f43b88e87691a4fd3eaba3288f5f3
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-02-29 17:47:41 +0100

    Create nameControl.js

---
+++ nameControl.js
@@ -0,0 +1,9 @@
+function nameControl(name) {
+    if (name === 'serra') {
+        console.log(`Ciao capitano, benvenuto! ^^`);
+    }
+    else if(name != 'serra'){
+        console.log('Ciao, sconosciuto ^^!')
+    }
+}
+let a = nameControl('Mario');
