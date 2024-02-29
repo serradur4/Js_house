6a4307108d918419648ac816daac9ffacf2af5c6
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-02-29 17:46:05 +0100

    Create arrayPrinting.js

---
+++ arrayPrinting.js
@@ -0,0 +1,7 @@
+function arrayPrinting(numbers){
+    for(let i = 0; i < numbers.length; i++){    //stampa un'array
+        console.log(numbers[i]);
+    }
+}
+const numbers =  [23, 24, 25, 26];
+const print = arrayPrinting(numbers)
