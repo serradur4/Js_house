03cd9068bf6c15fb3eb1ede98361a2511f1bdadd
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-03-03 16:51:25 +0100

    Delete nameControl.js

--- nameControl.js
+++
@@ -1,9 +0,0 @@
-function nameControl(name) {
-    if (name === 'serra') {
-        console.log(`Ciao capitano, benvenuto! ^^`);
-    }
-    else if(name != 'serra'){
-        console.log('Ciao, sconosciuto ^^!')
-    }
-}
-let a = nameControl('Mario');
